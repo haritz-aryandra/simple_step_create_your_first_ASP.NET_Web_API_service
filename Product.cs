6a34e57700d64b5b7183b29029a40e42462df6b8
haritz-aryandra <xpatriat82@live.co.uk>
2015-08-11 08:55:10 +0700

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,7 @@
+public class Product
+{
+    public int ProductID { get; set; }
+    public string ProductName { get; set; }
+    public string ProductCategory { get; set; }
+    public int Price { get; set; }
+}
